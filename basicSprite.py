6a0778d72181f2dcdad227519d7d895ef297af58
nabihestefan <estefan.nabih@gmail.com>
2020-04-08 19:47:16 -0400

    worked on code for enemies, pretty sure 2 of the 3 enemies should be done

--- basicSprite.py
+++ basicSprite.py
@@ -7,13 +7,12 @@ class Sprite(pygame.sprite.Sprite):
     This means this class will initialize images and center points for all sprites
     """
 
-    def __init__(self, centerPoint, image):
+    def __init__(self, centerPoint, images):
         """
         This will be the class that will initialize all of these basic values that were mentioned above
         """
         pygame.sprite.Sprite.__init__(self)
         """Set the image and the rect"""
-        self.image = image
-        self.rect = image.get_rect()
+        self.images = images
         """Move the rect into the correct position"""
         self.rect.center = centerPoint
--- monsters.py
+++ monsters.py
@@ -1,69 +1,245 @@
 import pygame
 import basicSprite
 import random
+import projectiles
 
 class Green_Monster(basicSprite.Sprite):
     """
     This is where we create the Green Monsters
+    Pretty sure this one is done
     """
-    def __init__(self, centerPoint, image):
+    def __init__(self, centerPoint, images, direction = random.randint(1,4), (x,y)):
         """
         use the initialization of the basic Sprite and the initialize any specific thigns for this enemy
         """
         basicSprite.Sprite.__init__(self, centerPoint, image)
         """start the basic initialization"""
+        self.original_rect = pygame.Rect(self.rect)
+        self.image_order = ["Down", "Left1", "Left2", "Right1", "Right2", "Up"]
 
+        self.coords = (x,y)
+
+        self.dead = False
+
+        self.attacking = False
+        """
+        Initialize the direction
+        1 = up
+        2 = Left
+        3 = right
+        4 = down
+        """
+        self.direction = direction
+        if self.direction == 1:
+            self.image = self.images[5]
+        elif self.direction == 2:
+            self.image = self.images[1]
+        elif self.direction == 3:
+            self.image = self.images[0]
+        elif self.direction == 4:
+            self.image = self.images[3]
+
+        self.dist = 1
+
+        self.thrown = False
+
+        self.step = 1
         pass
 
-    def update(self, block_group):
+    def update(self, block_group, character_coords, centerPoint, javelin_images):
         """
         This function is the one that will move an dupdate the position of the monsters
         It is called each "cycle" of gameplay to show that they move
         """
+        if self.dead:
+            break
+        xMove,yMove = 0,0
 
+        """First it check is the enemy can see the player. If he can, then the character will not move, it will throw the javelin"""
+        if character_coords[0] == self.coords[0]:#Check if enemy sees character horizontally
+            dif = character_coords[0] - self.coords[0]
+            if self.direction == 2 and diff > 0 and !self.thrown:#Enemy looking left and character is on the Left
+                throw_javelin(centerPoint, javelin_images)
+                self.thrown = True
+            elif self.direction == 4 and diff < 0 and !self.thrown:#Enemy looking left and character is on the Left
+                throw_javelin(centerPoint, javelin_images)
+                self.thrown = True
+        elif character_coords[1] == self.coords[1]:#Check if enemy sees character vertically
+            dif = character_coords[1] - self.coords[1]
+            if self.direction == 1 and diff < 0 and !self.thrown:#Enemy looking up and character is up
+                throw_javelin(centerPoint, javelin_images)
+                self.thrown = True
+            elif self.direction == 3 and diff > 0 and !self.thrown:#Enemy looking down and character is down
+                throw_javelin(centerPoint, javelin_images)
+                self.thrown = True
+        else:#If we didnt see the character, we move
+            if self.direction==1:#up
+                yMove = -self.dist
+            elif self.direction==2:#Left
+                xMove = self.dist
+                if self.step == 1:
+                    step = 2
+                    self.image = self.images[1]
+                if self.step == 2:
+                    step = 1
+                    self.image = self.images[2]
+            elif self.direction==3:#Down
+                yMove = self.dist
+            elif self.direction==4:#Right
+                xMove = -self.dist
+                if self.step == 1:
+                    step = 2
+                    self.image = self.images[3]
+                if self.step == 2:
+                    step = 1
+                    self.image = self.images[4]
+
+            self.rect.move_ip(xMove,yMove) #This is what actually moves the character
+            if pygame.sprite.spritecollideany(self, block_group):
+                """If we hit a block, don’t move – reverse the movement"""
+                self.rect.move_ip(-xMove,-yMove)
+                self.direction += 2
+                self.direction %= 4
         pass
 
-    def throw_javelin(self):
+    def throw_javelin(self, centerPoint, javelin_images):
         """
         This is the function that Throws the Javelin
         """
-
+        javelin = Javelin(centerPoint, javelin_images)
         pass
 
-    def die(self):
+    def javelin_fell(self):
+        """
+        This function tells the enemy he can now throw another javelin
+        """
+        self.thrown = False
+        pass
+
+    def die(self, ground_image):
         """
         This function establishes what happens when a enemy is killed
-        it will probably mean that the enemy is deleted and his spot in the game is replaced with a blank background spot
+        this means he is set do dead (so update cant be called) and the image is set as a ground image
         """
+        self.dead = True
+        self.images = ground_image
 
         pass
 
 class Bat(basicSprite.Sprite):
     """
     This is where we create the Bats
+    Pretty sure this one is done
     """
-    def __init__(self, centerPoint, image):
+    def __init__(self, centerPoint, images, direction = random.randint(1,4), (x,y)):
         """
         use the initialization of the basic Sprite and the initialize any specific thigns for this enemy
         """
         basicSprite.Sprite.__init__(self, centerPoint, image)
         """start the basic initialization"""
+        self.original_rect = pygame.Rect(self.rect)
+        self.image_order = ["Down1", "Down2", "Left1", "Left2", "Right1", "Right2", "Up1", "Up2"]
 
+        self.coords = (x,y)
+
+        self.dead = False
+
+        """
+        Initialize the direction
+        1 = up
+        2 = Left
+        3 = right
+        4 = down
+        """
+        self.direction = direction
+        if self.direction == 1:
+            self.image = self.images[6]
+        elif self.direction == 2:
+            self.image = self.images[2]
+        elif self.direction == 3:
+            self.image = self.images[0]
+        elif self.direction == 4:
+            self.image = self.images[4]
+
+        self.dist = 1
+
+        self.step = 1
         pass
 
-    def update(self, block_group):
+    def update(self, block_group, character_coords):
         """
         This function is the one that will move an dupdate the position of the monsters
         It is called each "cycle" of gameplay to show that they move
         """
+        if self.dead:
+            break
+        xMove,yMove = 0,0
 
-        pass
+        self.direction = random.randint(1,4)#set random way to move
 
-    def die(self):
+        """First it check is the enemy can see the player. If he can, then he will move towards the character"""
+        if character_coords[0] == self.coords[0] :#Check if enemy sees character horizontally
+            dif = character_coords[0] - self.coords[0]
+            if diff > 0:#Enemy looking left and character is on the Left
+                self.direction = 2
+            elif diff < 0:#Enemy looking right and character is on the right
+                self.direction = 4
+        elif character_coords[1] == self.coords[1]:#Check if enemy sees character vertically
+            dif = character_coords[1] - self.coords[1]
+            if diff < 0:#Enemy looking up and character is up
+                self.direction = 1
+            elif  diff > 0:#Enemy looking down and character is down
+                self.direction = 3
+
+        #either we move towards the character or we move randomly
+        if self.direction==1:#up
+            yMove = -self.dist
+            if self.step == 1:
+                step = 2
+                self.image = self.images[6]
+            if self.step == 2:
+                step = 1
+                self.image = self.images[7]
+        elif self.direction==2:#Left
+            xMove = self.dist
+            if self.step == 1:
+                step = 2
+                self.image = self.images[2]
+            if self.step == 2:
+                step = 1
+                self.image = self.images[3]
+        elif self.direction==3:#Down
+            yMove = self.dist
+            if self.step == 1:
+                step = 2
+                self.image = self.images[0]
+            if self.step == 2:
+                step = 1
+                self.image = self.images[1]
+        elif self.direction==4:#Right
+            xMove = -self.dist
+            if self.step == 1:
+                step = 2
+                self.image = self.images[4]
+            if self.step == 2:
+                step = 1
+                self.image = self.images[5]
+
+        self.rect.move_ip(xMove,yMove) #This is what actually moves the character
+        if pygame.sprite.spritecollideany(self, block_group):
+            """If we hit a block, don’t move – reverse the movement"""
+            self.rect.move_ip(-xMove,-yMove)
+            self.direction += 2
+            self.direction %= 4
+            pass
+
+    def die(self, ground_image):
         """
         This function establishes what happens when a enemy is killed
-        it will probably mean that the enemy is deleted and his spot in the game is replaced with a blank background spot
+        this means he is set do dead (so update cant be called) and the image is set as a ground image
         """
+        self.dead = True
+        self.images = ground_image
 
         pass
 
@@ -78,6 +254,8 @@ class Shooter(basicSprite.Sprite):
         """
         basicSprite.Sprite.__init__(self, centerPoint, image)
         """start the basic initialization"""
+        self.original_rect = pygame.Rect(self.rect)
+        self.image_order = ["Down1", "Down2","Left1","Left2","Right1","Right2","Up1","Up2"]
 
         pass
 
